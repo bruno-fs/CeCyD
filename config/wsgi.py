e4ef75dabfe64e9b289ecbe609e4f3cef54de139
Bruno FS <fsouza.bruno@gmail.com>
2020-03-04 12:34:34 -0300

    bugfix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(root_path)
+sys.path.insert(0, root_path)
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cellcycle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
